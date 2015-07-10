9c0f934346b45739ab248a052d677e13b48e33d6
jonathan1138 <jonathan1138@hotmail.com>
2015-07-10 15:34:40 -0400

    twelve

--- client/introReveal.js
+++ client/introReveal.js
@@ -4,6 +4,21 @@
     return Reveal.slide();
   };
 
+Reveal.initialize({
+
+    // Parallax background image
+    parallaxBackgroundImage: '2s', // e.g. "https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg"
+
+    // Parallax background size
+    parallaxBackgroundSize: '20px 20px', // CSS syntax, e.g. "2100px 900px" - currently only pixels are supported (don't use % or auto)
+
+    // Amount of pixels to move the parallax background per slide step,
+    // a value of 0 disables movement along the given axis
+    // These are optional, if they aren't specified they'll be calculated automatically
+    parallaxBackgroundHorizontal: 200,
+    parallaxBackgroundVertical: 50
+
+});
 /*
   Reveal.initialize({
     // Display controls in the bottom right corner
